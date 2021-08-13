c3e78ea9a214df23d08fc003c0261fa24d402ce0
Haeil Lim <lhi7837@gmail.com>
2021-08-13 15:55:44 +0900

    test

---
+++ pr42895.kt
@@ -0,0 +1,9 @@
+// https://programmers.co.kr/learn/courses/30/lessons/42895#
+
+class Solution {
+    fun solution(N: Int, number: Int): Int {
+        var answer = 0
+
+        return answer
+    }
+}
